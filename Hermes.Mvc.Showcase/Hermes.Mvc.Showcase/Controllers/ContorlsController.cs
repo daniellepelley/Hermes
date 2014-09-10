585048dbe54cb211300cc53cb2ee4625ecd7c622
Daniel Le Pelley <daniellepelley@gmail.com>
2014-09-10 23:06:56 +0100

    Built a demo form Added tiles and carousel

---
+++ Hermes.Mvc.Showcase/Hermes.Mvc.Showcase/Controllers/ContorlsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace Hermes.Mvc.Showcase.Controllers
+{
+    public class ControlsController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Tiles");
+        }
+
+                public ActionResult Tiles()
+        {
+            return View();
+        }
+
+                public ActionResult Carousel()
+        {
+            return View();
+        }
+
+        
+
+    }
+}
---
+++ Hermes.Mvc.Showcase/Hermes.Mvc.Showcase/Scripts/Form/app.js
@@ -0,0 +1,40 @@
+﻿var app = angular.module('formApp', []);
+
+
+app.controller('formController', function($scope) {
+    $scope.title = "";
+    $scope.firstname = "Daniel";
+    $scope.surname = "Le Pelley";
+    $scope.age = 38;
+});
+
+app.controller('exchangeController', function ($scope, dataService) {
+    $scope.count = 0;
+    $scope.rate = null;
+
+    var update = function () {
+        dataService.getData(function (dataResponse) {
+            $scope.rate = dataResponse[1].Rate;
+            $scope.count += 1;
+        });
+    };
+
+    setInterval(update, 5000);
+});
+
+
+app.factory('dataService', function ($http) {
+    return {
+        getData: function (callbackFunc) {
+            $http({
+                method: 'GET',
+                url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22EURUSD%22%2C%22GBPUSD%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
+            }).success(function (data) {
+                callbackFunc(data.query.results.rate);
+
+            }).error(function () {
+                alert("error");
+            });
+        }
+    };
+});
