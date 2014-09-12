9edc1e123d6f4cec4bcb83b6ab80ff93f7ca7b9d
Daniel Le Pelley <daniellepelley@gmail.com>
2014-09-12 07:40:52 +0100

    Added new angular project and tests

---
+++ Hermes.Mvc.Angular.Test/RulesToAngularAttributesTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hermes.Validation.Interfaces;
+using Hermes.Validation.Rules.Preset.Numeric;
+using Hermes.Validation.Rules.Preset.String;
+using NUnit;
+using NUnit.Framework;
+
+namespace Hermes.Mvc.Angular.Test
+{
+    
+    public class RulesToAngularAttributesTest
+    {
+        [Test]
+        public void MaximumLengthRuleAttributeName()
+        {
+            var rule = new MaximumLengthRule(5);
+            var sut = new RulesToAttributeConverter();
+
+            var actual = sut.CreateAttribute(rule)[0];
+
+            Assert.AreEqual("maxlength", actual);
+        }
+
+        [Test]
+        public void MaximumLengthRuleAttributeValue()
+        {
+            var rule = new MaximumLengthRule(5);
+            var sut = new RulesToAttributeConverter();
+
+            var actual = sut.CreateAttribute(rule)[1];
+
+            Assert.AreEqual("5", actual);
+        }
+
+        [Test]
+        public void MinRuleAttributeName()
+        {
+            var rule = new MinimumRule(6);
+            var sut = new RulesToAttributeConverter();
+
+            var actual = sut.CreateAttribute(rule)[0];
+
+            Assert.AreEqual("min", actual);
+        }
+
+        [Test]
+        public void MinRuleAttributeValue()
+        {
+            var rule = new MinimumRule(6);
+            var sut = new RulesToAttributeConverter();
+
+            var actual = sut.CreateAttribute(rule)[1];
+
+            Assert.AreEqual("6", actual);
+        }
+
+        [Test]
+        public void MaxRuleAttributeName()
+        {
+            var rule = new MaximumRule(7);
+            var sut = new RulesToAttributeConverter();
+
+            var actual = sut.CreateAttribute(rule)[0];
+
+            Assert.AreEqual("max", actual);
+        }
+
+        [Test]
+        public void MaxRuleAttributeValue()
+        {
+            var rule = new MaximumRule(7);
+            var sut = new RulesToAttributeConverter();
+
+            var actual = sut.CreateAttribute(rule)[1];
+
+            Assert.AreEqual("7", actual);
+        }
+
+        [Test]
+        public void CreateAttributes()
+        {
+            var rules = new IRule[]
+            {
+                new MinimumRule(6),
+                null,
+                new MaximumRule(7),
+                new MaximumLengthRule(5),
+                null
+            };
+
+            var sut = new RulesToAttributeConverter();   
+
+            var dictionary = new Dictionary<string, string>
+            {
+                {"min", "6"},
+                {"max", "7"},
+                {"maxlength", "5"}
+            };
+
+            var actual = sut.CreateAttributes(rules);
+
+            CollectionAssert.AreEquivalent(dictionary,actual);
+        }
+    }
+}
